e17ef3234fc500bfb3fa96b078db1435b27f51fc
Gabriel Saldanha <gabrielcrsaldanha@gmail.com>
2023-12-02 11:48:29 -0300

    Add e2e test for invalid requests to API

--- tests/e2e/cast_member_api/test_create.py
+++ tests/e2e/cast_member_api/test_create.py
@@ -17,3 +17,15 @@ class TestCreateCastMemberView:
 
         assert response.status_code == 201
         assert response.data == {"id": response.data["id"]}
+
+    def test_create_cast_member_with_post_payload_data_with_invalid_cast_member_type(self) -> None:
+        response = APIClient().post(
+            "/api/cast_members/",
+            data={
+                "name": "John Doe",
+                "cast_member_type": "INVALID",
+            },
+        )
+
+        assert response.status_code == 400
+        assert response.data == {"cast_member_type": ["\"INVALID\" is not a valid choice."]}
--- tests/e2e/genre_api/test_create.py
+++ tests/e2e/genre_api/test_create.py
@@ -18,3 +18,16 @@ class TestCreateGenreView:
 
         assert response.status_code == 201
         assert response.data == {"id": response.data["id"]}
+
+    def test_create_genre_with_empty_name(self, category_drama: Category) -> None:
+        response = APIClient().post(
+            "/api/genres/",
+            data={
+                "name": "",
+                "description": "Genre for action",
+                "categories": [str(category_drama.id)],
+            },
+        )
+
+        assert response.status_code == 400
+        assert response.data == {"name": ["This field may not be blank."]}
